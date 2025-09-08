e37098a162f003a56014b2268544f810ff0c3afc
sava <savaneli.david@gmail.com>
2025-09-08 17:53:03 +0400

    test

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./CatchIt";
-import { createHashRouter, RouterProvider } from "react-router-dom";
-
-const router = createHashRouter(
-  [
-    {
-      path: "/games/catchit/:gameId",
-      element: <App />,
-    },
-    {
-      path: "/",
-      element: <App />,
-    },
-  ],
-  { future: { v7_startTransition: true } }
-);
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/games/catchit/:gameId" element={<App />} />
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
