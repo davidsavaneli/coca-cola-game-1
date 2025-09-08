9087d31be38546f42ae5c48d08e6e3730a7282e7
sava <savaneli.david@gmail.com>
2025-09-08 18:20:19 +0400

    test

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./CatchIt";
-import Layout from "./Layout";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<App />} />
-          <Route path="games/catchit/:gameId" element={<App />} />
-        </Route>
+        <Route path="/games/catchit/:gameId" element={<App />} />
+        <Route path="/" element={<App />} />
+        <Route path="/test" element={<>test</>} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
